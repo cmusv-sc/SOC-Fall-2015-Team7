246cd126235dc98dd278ba0287b57342cb4a3575
xuehung <xuehung@gmail.com>
2015-11-20 17:32:25 -0500

    2nd

--- ApacheCMDA-Frontend/app/controllers/WorkflowController.java
+++ ApacheCMDA-Frontend/app/controllers/WorkflowController.java
@@ -18,30 +18,29 @@ import util.APICall.ResponseType;
 import views.html.climate.*;
 
 public class WorkflowController extends Controller {
-	
+
 	final static Form<Workflow> workflowForm = Form.form(Workflow.class);
 
 	public static Result home(int page) {
 		return ok(forum.render(Workflow.page(page), page, Workflow.getNumPage()));
 	}
-	
+
 	public static Result workflow(long id) {
 		return ok(workflow.render(Workflow.one(id), null));
 	}
-	
+
 	public static Result addWorkflow() {
 		return ok(addWorkflow.render(workflowForm));
 	}
-	
+
 	public static Result newWorkflow() {
 		Form<Workflow> dc = workflowForm.bindFromRequest();
 		ObjectNode jsonData = Json.newObject();
 		try {
-			jsonData.put("author", "zhongchuan");
-			jsonData.put("authorId", "1");
-			
+			jsonData.put("author", session("userName"));
+			jsonData.put("authorId", session("id"));
 			jsonData.put("name", dc.field("Name").value());
-			jsonData.put("purpose", dc.field("Purpose").value());			
+			jsonData.put("purpose", dc.field("Purpose").value());
 			jsonData.put("input", dc.field("Input").value());
 			jsonData.put("output", dc.field("Output").value());
 			jsonData.put("contributors", dc.field("Contributors").value());
@@ -68,5 +67,5 @@ public class WorkflowController extends Controller {
 		}
 		return redirect("/workflow/new/workflow");
 	}
-	
+
 }
