fb0c2f24bb8c179e5c29b7d31619e7d3da38dbc2
Yiming Zhang <lele5842010@gmail.com>
2015-12-02 10:03:48 -0500

    Added backend for subscription.

---
+++ ApacheCMDA-Backend/app/controllers/SubscriptionController.java
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Subscription;
+import models.SubscriptionRepository;
+import play.mvc.*;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.persistence.PersistenceException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.gson.Gson;
+
+/**
+ * The main set of web services.
+ */
+@Named
+@Singleton
+public class SubscriptionController extends Controller {
+
+	private final SubscriptionRepository subscriptionRepository;
+
+	// // We are using constructor injection to receive a repository to support our
+	// // desire for immutability.
+	@Inject
+	public SubscriptionController(final SubscriptionRepository subscriptionRepository) {
+		this.subscriptionRepository = subscriptionRepository;
+	}
+
+	public Result test() {
+		System.out.println("test called");
+		return badRequest("Zhaige SB");
+	}
+
+	public Result addSubscription() {
+		JsonNode json = request().body().asJson();
+		if (json == null) {
+			System.out.println("Subscription not added, expecting Json data");
+			return badRequest("Subscription not added, expecting Json data");
+		}
+
+		// Parse JSON file
+		long userId = json.path("userId").asLong();
+		String subscriptTargetClass = json.path("subscriptTargetClass").asText();
+		long targetId = json.path("targetId").asLong();
+
+		try {
+			// if (subscriptionRepository.findByUserIdAndSubscriptTargetClassAndTargetId(userId, subscriptTargetClass, targetId)>0) {
+			// 	System.out.println("Subscription has already recorded before");
+			// 	return badRequest("Duplicated request");
+			// }
+			Subscription subs = new Subscription(userId, subscriptTargetClass, targetId);
+			subscriptionRepository.save(subs);
+			System.out.println("Subscription saved");
+			return created(new Gson().toJson(subs.getId()));
+		} catch (PersistenceException pe) {
+			pe.printStackTrace();
+			System.out.println("Subscription not saved: " + userId + " " + subscriptTargetClass + " " + targetId);
+			return badRequest("Subscription not saved: " + userId + " " + subscriptTargetClass + " " + targetId);
+		}
+	}
+
+	public Result getSubscriptionByUserID(Long id, String targetClass, String format) {
+		if (id == null || targetClass == null) {
+			System.out.println("User id/TargetClass is null or empty!");
+			return badRequest("User id/TargetClass is null or empty!");
+		}
+
+		if (!targetClass.equals("Workflow") && !(targetClass.equals("User"))) {
+			System.out.println("TargetClass is not valid!");
+			return badRequest("TargetClass is not valid!");
+		}
+
+		List<Subscription> subs = subscriptionRepository.findByUserIdAndSubscriptTargetClass(id, targetClass);
+
+		if (subs.size() == 0) {
+			System.out.println("Subscription not found");
+			return notFound("Subscription not found");
+		}
+		String result = new String();
+		if (format.equals("json")) {
+			result = new Gson().toJson(subs.get(0));
+		}
+		return ok(result);
+	}
+	
+}
---
+++ ApacheCMDA-Frontend/app/controllers/SubscriptionController.java
@@ -0,0 +1,74 @@
+package controllers;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import models.metadata.ClimateService;
+import models.metadata.Workflow;
+import models.metadata.User;
+import play.data.Form;
+import play.libs.Json;
+import play.mvc.*;
+import util.APICall;
+import util.APICall.ResponseType;
+import views.html.climate.*;
+
+
+public class SubscriptionController extends Controller {
+	
+	public static Result getSubscripterList(Long userID) {
+		// return ok(subscription.render(User.one(userID), Subsciption.getSubscripterList(UserID));
+		return ok(userProfile.render(User.one(userID), User.getUserWorkflows(userID)));
+	}
+
+	public static Result getSubscripteeList(Long userID) {
+		return ok(userProfile.render(User.one(userID), User.getUserWorkflows(userID)));
+		// return ok(subscription.render(User.one(userID), Subsciption.getSubscripteeList(UserID)));
+	}
+
+	// public static Result home(int page) {
+	// 	return ok(forum.render(Workflow.page(page), page, Workflow.getNumPage()));
+	// }
+	
+	// public static Result workflow(long id) {
+	// 	return ok(workflow.render(Workflow.one(id), null));
+	// }
+	
+	// public static Result addWorkflow() {
+	// 	return ok(addWorkflow.render(workflowForm));
+	// }
+	
+	// public static Result newWorkflow() {
+	// 	Form<Workflow> dc = workflowForm.bindFromRequest();
+	// 	ObjectNode jsonData = Json.newObject();
+	// 	try {
+
+	// 		String originalName = dc.field("Name").value();
+	// 		String newName = originalName.replace(' ', '-');
+
+	// 		if (newName != null && !newName.isEmpty()) {
+	// 			jsonData.put("name", newName);
+	// 		}
+	// 		jsonData.put("purpose", dc.field("Purpose").value());
+	// 		jsonData.put("versionNo", dc.field("Version").value());
+	// 		jsonData.put("rootWorkflowId", dc.field("Root_Service").value());
+	// 		jsonData.put("userSet", dc.field("User_Set").value());
+	// 		jsonData.put("climateServiceSet", dc.field("ClimateService_Set").value());
+
+	// 		JsonNode response = Workflow.create(jsonData);
+	// 		Application.flashMsg(response);
+	// 	} catch (IllegalStateException e) {
+	// 		e.printStackTrace();
+	// 		Application.flashMsg(APICall
+	// 				.createResponse(ResponseType.CONVERSIONERROR));
+	// 	} catch (Exception e) {
+	// 		e.printStackTrace();
+	// 		Application.flashMsg(APICall.createResponse(ResponseType.UNKNOWN));
+	// 	}
+	// 	return redirect("/workflow/new/workflow");
+	// }
+}
